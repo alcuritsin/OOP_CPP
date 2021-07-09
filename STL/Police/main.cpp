5a032cf114272ec9d96971c61ce3a9a686b40e06
alcuritsin <al.curitsin@gmail.com>
2021-07-09 21:23:52 +0500

    Police -- done Не идеально. Нужно избавиться от лишних пробелов. Возможно не лучшая идея использовать CSV

--- STL/Police/main.cpp
+++ STL/Police/main.cpp
@@ -4,6 +4,7 @@
 #include<map>
 #include<Windows.h>
 #include<fstream>
+#include<algorithm>
 
 const std::string base_name = "base";
 
@@ -26,24 +27,24 @@ void main()
 
 
 
-	std::map<std::string, std::list<std::string>> base =
+	std::map<std::string, std::list<std::string>> base; /* =
 	{
 		std::pair<std::string, std::list<std::string>>("m123mm777", {"Проезд на красный", "Привышение скорости"}),
 		std::pair<std::string, std::list<std::string>>("m446mm011", {"Парковка в не положенном месте"}),
 		std::pair<std::string, std::list<std::string>>("k123vk011", {"Езда в нетрезвом состоянии", "Оскорбление сотрудника полиции", "Оказание сопротивления при аресте"})
-	};
+	};*/
 
 	load_base(base);
 
 	print_base(base);
-	save_base(base);
+	//save_base(base);
 }
 
 void print_base(const std::map<std::string, std::list<std::string>>& base)
 {
 	for (std::map<std::string, std::list<std::string>>::const_iterator it = base.cbegin(); it != base.cend(); it++)
 	{
-		std::cout << it->first << "\t";
+		std::cout << it->first << ":\t";
 		for (std::list<std::string>::const_iterator ij = it->second.cbegin(); ij != it->second.cend(); ij++)
 		{
 			std::cout << *ij;
@@ -62,7 +63,7 @@ void save_base(const std::map<std::string, std::list<std::string>>& base)
 	std::ofstream fout(base_name);
 	for (std::map<std::string, std::list<std::string>>::const_iterator it = base.cbegin(); it != base.cend(); it++)
 	{
-		fout << it->first << "\t";
+		fout << it->first << ":\t";
 		for (std::list<std::string>::const_iterator ij = it->second.cbegin(); ij != it->second.cend(); ij++)
 		{
 			fout << *ij;
@@ -75,6 +76,8 @@ void save_base(const std::map<std::string, std::list<std::string>>& base)
 	}
 	fout.close();
 
+
+
 	std::string	command = "start notepad " + base_name;
 	system(command.c_str());
 }
@@ -85,20 +88,37 @@ void load_base(std::map<std::string, std::list<std::string>>& base)
 
 	std::string id;
 	std::string crime;
+	std::string crimes;
 	if (fin.is_open())
 	{
-		while (!fin.eof())
+ 		while (!fin.eof())
 		{
+
 			std::getline(fin, id, ':');
+			//if (id.size() < 2) continue;
 			//do
 			//{
 			//	std::getline(fin, crime, ',');
 			//	base[id].push_back(crime);
 			//} while (crime.back() != ';');
-			while (std::getline(fin, crime,','))
+			// 
+			/*while (std::getline(fin, crime,','))
 			{
 				base[id].push_back(crime);
+			}*/
+
+			std::getline(fin, crimes, ';');
+			fin.ignore();
+			for (int i = 0, next_coma = i; i < crimes.size(); i = next_coma)
+			{
+				next_coma = crimes.find(',', i+1);
+				std::string crime(crimes, i+1 , (next_coma>0? next_coma - i - 1:crimes.size()-i));
+				std::cout << ":"<<crime<<":" << "\n";
+				base[id].push_back(crime);
 			}
+			
+
+
 		}
 	}
 	else
